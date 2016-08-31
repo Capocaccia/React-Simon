2dd5c798b6873f0d15f6de1e38914edcb186e483
Carter Capocaccia <carter.capocaccia@gmail.com>
2016-08-31 13:15:53 -0500

    Refactors react structure into component parts

---
+++ src/components/ColorBoxes.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+export default class ColorBoxes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: this.props.initialColor
+    }
+  }
+  handleClick(color){
+    this.setState({
+      color: color
+    })
+  }
+  scrollColors(colors) {
+    this.interval = setInterval(() => {
+      if(count < colors.length){
+        this.handleClick(colors[count].backgroundColor);
+      } else {
+        count = 0;
+        this.handleClick(colors[count].backgroundColor);
+      }
+      count++;
+    }, 1000);
+  }
+  endScrollColors(){
+    clearInterval(this.interval);
+  }
+  render(){
+    // this
+    var ColorBox = this.props.colorList.map((color, i) => {
+      return (
+          <div onClick={ () => this.handleClick(color.backgroundColor)} key={i} style={{'height' : '100px', 'width': '100px', 'backgroundColor': color.backgroundColor}}>
+          </div>
+      )
+    });
+    return (
+        <div>
+          <h2 style={{color: this.state.color}}>Click a box and I change</h2>
+          <h3>Color Selected: {this.state.color}</h3>
+          {ColorBox}
+          <button onClick={() => this.scrollColors(colorList)}>Cycle through colors</button>
+          <button onClick={() => this.endScrollColors()}>Stop Cycling</button>
+        </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import ColorBoxes from './components/ColorBoxes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var colorList = [
+    {backgroundColor: 'yellow'},
+    {backgroundColor: 'blue'},
+    {backgroundColor: 'red'}
+];
+var count = 0;
+
+
+
+class Text extends React.Component {
+    render(){
+        return (
+            <div>
+                <ColorBoxes initialColor="purple" colorList={this.props.colorList} />
+            </div>
+        )
+    }
+}
+
+
+
+ReactDOM.render(
+    <Text colorList={colorList}  />,
+    document.getElementById('root')
+);
+
